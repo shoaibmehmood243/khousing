bed10640158223b178b9914567cc105789fa05c7
shoaibmehmood243 <shoaibmehmood065@gmail.com>
2023-05-31 23:45:33 +0500

    dwola

--- src/Controllers/payments.controller.js
+++ src/Controllers/payments.controller.js
@@ -87,35 +87,35 @@ const paymentController = {
                 amount: req.body.amount,
                 payment_id: req.body.payment_id
             }
-            const paymentData = await Payment.getPaymentById(transactionData.payment_id);
-            var requestBody = {
-                _links: {
-                  source: {
-                    href: req.body.funding_source,
-                  },
-                  destination: {
-                    href: paymentData[0].funding_source,
-                  },
-                },
-                amount: {
-                  currency: "USD",
-                  value: transactionData.amount,
-                },
-            }
-            dwolla.post('transfers', requestBody)
-                .then((dRes)=> {
-                    res.status(200).send({status: true,message: 'Something went wrong.'});
-                })
-                .catch((err)=> {
-                    next(err);
-                })
-            // const transactionObj = new Transactions(transactionData);
-            // const response = await Payment.RecordPayment(transactionObj);
-            // if(response) {
-            //     res.status(200).send({status: true,message: 'Transaction logged successfully.'});
-            // } else {
-            //     res.status(200).send({status: false,message: 'Something went wrong.'});
+            // const paymentData = await Payment.getPaymentById(transactionData.payment_id);
+            // var requestBody = {
+            //     _links: {
+            //       source: {
+            //         href: req.body.funding_source,
+            //       },
+            //       destination: {
+            //         href: paymentData[0].funding_source,
+            //       },
+            //     },
+            //     amount: {
+            //       currency: "USD",
+            //       value: transactionData.amount,
+            //     },
             // }
+            // dwolla.post('transfers', requestBody)
+            //     .then((dRes)=> {
+            //         res.status(200).send({status: true,message: 'Something went wrong.'});
+            //     })
+            //     .catch((err)=> {
+            //         next(err);
+            //     })
+            const transactionObj = new Transactions(transactionData);
+            const response = await Payment.RecordPayment(transactionObj);
+            if(response) {
+                res.status(200).send({status: true,message: 'Transaction logged successfully.'});
+            } else {
+                res.status(200).send({status: false,message: 'Something went wrong.'});
+            }
         } catch (error) {
             next(error);
         }
